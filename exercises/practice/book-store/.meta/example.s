e7e12dbcf129c256a1020c3a705fb39fbe07d186
Eric Willigers <keiravillekode@gmail.com>
2025-08-23 21:31:19 +1000

    Add book-tore - tidy

--- exercises/practice/book-store/.meta/example.s
+++ exercises/practice/book-store/.meta/example.s
@@ -8,14 +8,14 @@ count:
         beqz    a0, .return
 
 .loop:
-        addi     a0, a0, -2
+        addi    a0, a0, -2
         add     t5, a1, a0
-        lhu    t0, 0(t5)           /* book */
-        slli     t0, t0, 1
+        lhu     t0, 0(t5)               /* book */
+        slli    t0, t0, 1
         add     t5, a2, t0
-        lhu    t1, 0(t5)
-        addi    t1, t1, 1            /* increment tally */
-        sh    t1, 0(t5)
+        lhu     t1, 0(t5)
+        addi    t1, t1, 1               /* increment tally */
+        sh      t1, 0(t5)
         bnez    a0, .loop
 
 .return:
@@ -23,58 +23,52 @@ count:
 
 
 
-
 /* void sort(uint16_t *tally) */
 sort:
-        addi   t0, a0, 4
-        addi   t1, a0, 12               /* offsets 4..10 for books 2..5 */
+        addi    t0, a0, 4
+        addi    t1, a0, 12              /* offsets 4..10 for books 2..5 */
 
 .outer:
-        lhu    t2, 0(t0)
-        move   t3, t0                 /* offset for inner loop of insertion sort */
+        lhu     t2, 0(t0)
+        move    t3, t0                  /* offset for inner loop of insertion sort */
 
 .inner:
         addi    t3, t3, -2
         beq     t3, a0, .exit
 
-        lhu    t4, 0(t3)
-        bge    t4, t2, .exit
+        lhu     t4, 0(t3)
+        bge     t4, t2, .exit
 
         sh      t4, 2(t3)
         j       .inner
 
 .exit:
-        sh    t2, 2(t3)
-
+        sh      t2, 2(t3)
         addi    t0, t0, 2
         bne     t0, t1, .outer
 
         jalr    zero, 0(t6)             /* return */
 
 
-
-
-
 /* void difference(uint16_t *tally); */
 difference:
-        lh    t0, 10(a0)
-        lh    t1, 8(a0)
+        lh      t0, 10(a0)
+        lh      t1, 8(a0)
         sub     t3, t1, t0
-        sh    t3, 8(a0)           /* tally[4] - tally[5] */
+        sh      t3, 8(a0)               /* tally[4] - tally[5] */
 
-        lh    t0, 6(a0)
+        lh      t0, 6(a0)
         sub     t3, t0, t1
-        sh    t3, 6(a0)           /* tally[3] - tally[4] */
+        sh      t3, 6(a0)               /* tally[3] - tally[4] */
 
-        lh    t1, 4(a0)
+        lh      t1, 4(a0)
         sub     t3, t1, t0
-        sh    t3, 4(a0)           /* tally[2] - tally[3] */
+        sh      t3, 4(a0)               /* tally[2] - tally[3] */
 
-        lh    t0, 2(a0)
+        lh      t0, 2(a0)
         sub     t3, t0, t1
-        sh    t3, 2(a0)           /* tally[1] - tally[2] */
+        sh      t3, 2(a0)               /* tally[1] - tally[2] */
         jalr    zero, 0(t6)             /* return */
-
 
 
 /* void adjust(uint16_t *tally); */
@@ -90,41 +84,40 @@ adjust:
 
 .adjust:
         sub     t0, t0, t3
-        sh     t0, 6(a0)
+        sh      t0, 6(a0)
         add     t1, t1, t3
         add     t1, t1, t3
-        sh     t1, 8(a0)
+        sh      t1, 8(a0)
         sub     t2, t2, t3
-        sh     t2, 10(a0)
+        sh      t2, 10(a0)
 
         jalr    zero, 0(t6)             /* return */
 
 
-
 /* uint32_t score(uint16_t *tally); */
 score:
-        lhu    t1, 2(a0)
-        li     t0, 800               /* price of 1 book */
+        lhu     t1, 2(a0)
+        li      t0, 800                 /* price of 1 book */
         mul     t2, t0, t1
 
-        lhu    t1, 4(a0)
-        li     t0, 1520              /* price of 2 books */
-        mul    t3, t0, t1
+        lhu     t1, 4(a0)
+        li      t0, 1520                /* price of 2 books */
+        mul     t3, t0, t1
         add     t2, t2, t3
 
-        lhu    t1, 6(a0)
-        li     t0, 2160              /* price of 3 books */
-        mul    t3, t0, t1
+        lhu     t1, 6(a0)
+        li      t0, 2160                /* price of 3 books */
+        mul     t3, t0, t1
         add     t2, t2, t3
 
-        lhu    t1, 8(a0)
-        li     t0, 2560              /* price of 4 books */
-        mul    t3, t0, t1
+        lhu     t1, 8(a0)
+        li      t0, 2560                /* price of 4 books */
+        mul     t3, t0, t1
         add     t2, t2, t3
 
-        lhu    t1, 10(a0)
-        li     t0, 3000              /* price of 5 books */
-        mul    t3, t0, t1
+        lhu     t1, 10(a0)
+        li      t0, 3000                /* price of 5 books */
+        mul     t3, t0, t1
         add     a0, t2, t3
 
         jalr    zero, 0(t6)             /* return */
@@ -132,10 +125,16 @@ score:
 
 /* extern void total(size_t basket_count, const uint16_t *basket, uint16_t *tally); */
 total:
+        addi    sp, sp, -16
+        sw      zero, 0(sp)
+        sw      zero, 4(sp)
+        sw      zero, 8(sp)
+        move    a2, sp
         jal     t6, count
         move    a0, a2
         jal     t6, sort
         jal     t6, difference
         jal     t6, adjust
         jal     t6, score
+        addi    sp, sp, 16
         ret
--- generators/exercises/book_store.py
+++ generators/exercises/book_store.py
@@ -6,9 +6,7 @@ FUNC_PROTO = """\
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-extern uint32_t total(size_t basket_count, const uint16_t *basket, uint16_t *tally);
-
-/* extern uint32_t total_true(size_t basket_count, const uint16_t *basket); */
+extern uint32_t total(size_t basket_count, const uint16_t *basket);
 """
 
 
@@ -17,22 +15,6 @@ def array_literal(numbers):
 
 
 def gen_func_body(prop, inp, expected):
-    basket = inp["basket"]
-    str_list = []
-    str_list.append("uint16_t tally[6] = { 0, 0, 0, 0, 0, 0 };\n")
-
-    if len(basket) > 0:
-        str_list.append(f"const uint16_t basket[] = {array_literal(basket)};\n")
-        str_list.append(
-            f"TEST_ASSERT_EQUAL_UINT32({expected}, {prop}(ARRAY_SIZE(basket), basket, tally));\n"
-        )
-    else:
-        str_list.append(f"TEST_ASSERT_EQUAL_UINT32({expected}, {prop}(0, NULL, tally));\n")
-
-
-    return "".join(str_list)
-
-def gen_func_body_true(prop, inp, expected):
     basket = inp["basket"]
     str_list = []
     if len(basket) > 0:
