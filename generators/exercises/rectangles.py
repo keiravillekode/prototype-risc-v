01512f4395729a867596d1d938c359fdcf0c0cea
Eric Willigers <keiravillekode@gmail.com>
2026-07-15 11:32:07 +1000

    add `rectangles` exercises

---
+++ generators/exercises/rectangles.py
@@ -0,0 +1,56 @@
+FUNC_PROTO = """\
+#include "vendor/unity.h"
+
+#include <stddef.h>
+
+extern int rectangles(const char **strings);
+"""
+
+
+def extra_cases():
+    return [
+        {
+            "description": "very large input",
+            "property": "rectangles",
+            "input": {
+                "strings": [
+                    "      +-----+--------+    +-----+ ",
+                    "++---++-----+--------+---++-----++",
+                    "||+--++-----+-+-++   |   ||     ||",
+                    "|||  ||     +-+-++-+ |   ||     ||",
+                    "|||  ||     | | || | |   ||     ||",
+                    "||| +++-----+-+-++-+-+---++-+   ||",
+                    "||| |||     | | || | |+--++-+-+ ||",
+                    "||| +++---+-+-+-++-+-++--++-+ | ||",
+                    "||| |||+--+-+-+-+| | |+--++---+ ||",
+                    "||| ||||  | | | || | |+-+||     ||",
+                    "||+-++++--+-+++-++-+-++-+++---++||",
+                    "||  |+++--+-+++-+--+-+| |||   ||||",
+                    "+++-+++++---++--+-++-++-+++---+|||",
+                    " |+-+++++---++--+ || || |||   ||||",
+                    " |  +++++---++--+-++-++-++++  ||||",
+                    " |   ||||   |+----++-++-++++--+++|",
+                    " |   |+++---+|    || || ||    || |",
+                    "+++  |||+---++----+| || ||    || |",
+                    "|||  +++----++----++-++-++----++-+",
+                    "+++---++----++-----+-++-++----++  ",
+                    "                      +-+         ",
+                ]
+            },
+            "expected": 2063,
+        }
+    ]
+
+
+def gen_func_body(prop, inp, expected):
+    strings = inp["strings"]
+    str_list = []
+    str_list.append("const char *strings[] = {")
+    str_list.append("    // clang-format off")
+    for line in strings:
+        str_list.append(f'    "{line}",')
+    str_list.append("    NULL")
+    str_list.append("    // clang-format on")
+    str_list.append("};")
+    str_list.append(f"TEST_ASSERT_EQUAL_INT({expected}, {prop}(strings));\n")
+    return "\n".join(str_list)
